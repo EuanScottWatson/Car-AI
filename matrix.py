93c0643a0ff95970f269de0abcbc832765afb553
Euan Imperial <es1519@ic.ac.uk>
2020-02-24 18:34:37 +0000

    Finished Matrix class.

--- matrix.py
+++ matrix.py
@@ -1,5 +1,6 @@
 import numpy as np
 import random
+import copy
 
 
 class Matrix:
@@ -19,12 +20,64 @@ class Matrix:
     def multiply_scalar(self, n):
         self.matrix *= n
 
-    def dot_matrix(self, matrix):
-        return self.matrix.dot(matrix.matrix)
+    def add(self, n):
+        self.matrix += n
 
+    def subtract(self, n):
+        self.matrix -= n
 
-matrix = Matrix(3, 4)
-matrix.randomise()
-matrix2 = Matrix(4, 3)
-matrix2.randomise()
-print(matrix.dot_matrix(matrix2))
+    def multiply_matrix(self, n):
+        return np.multiply(self.matrix, n)
+
+    def dot_matrix(self, n):
+        return self.matrix.dot(n.matrix)
+
+    def transpose(self):
+        return self.matrix.transpose()
+
+    def activate(self):
+        newMatrix = Matrix(self.rows, self.cols)
+        for i in range(self.rows):
+            for j in range(self.cols):
+                newMatrix.matrix[i][j] = self.sigmoid(self.matrix[i][j])
+        return newMatrix
+
+    def sigmoid(self, x):
+        return 1 / (1 + np.e ** (-1 * x))
+
+    def sigmoid_prime(self, x):
+        newMatrix = Matrix(self.rows, self.cols)
+        for i in range(self.rows):
+            for j in range(self.cols):
+                newMatrix.matrix[i][j] = self.matrix[i][j] * (1 - self.matrix[i][j])
+        return newMatrix
+
+    def clone(self):
+        return copy.deepcopy(self.matrix)
+
+    def mutate(self, mutationRate):
+        for i in range(self.rows):
+            for j in range(self.cols):
+                rand = random.random()
+                if rand < mutationRate:
+                    self.matrix[i][j] += np.random.normal() / 5
+
+                if self.matrix[i][j] > 1:
+                    self.matrix[i][j] = 1
+                if self.matrix[i][j] < -1:
+                    self.matrix[i][j] = -1
+
+    def crossover(self, matrixB):
+        child = Matrix(self.rows, self.cols)
+
+        rowCrossOver = random.randint(0, self.rows - 1)
+        colCrossOver = random.randint(0, self.cols - 1)
+
+        for i in range(self.rows):
+            for j in range(self.cols):
+                if (i < rowCrossOver) or (i == rowCrossOver and j <= colCrossOver):
+                    child.matrix[i][j] = self.matrix[i][j]
+                else:
+                    child.matrix[i][j] = matrixB.matrix[i][j]
+
+        return child
